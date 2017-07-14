99b5973aba88192020ed3fb7fe2c07aa2cb278d1
Tim Burke <tim.burke@gmail.com>
2017-07-14 00:22:56 +0000

    Move backend and errorcode lookups to separate file

--- backend.go
+++ backend.go
@@ -10,56 +10,6 @@ import "errors"
 import "fmt"
 import "unsafe"
 
-func nameToId(name string) (C.ec_backend_id_t, error) {
-	switch name {
-	case "null":
-		return C.EC_BACKEND_NULL, nil
-	case "jerasure_rs_vand":
-		return C.EC_BACKEND_JERASURE_RS_VAND, nil
-	case "jerasure_rs_cauchy":
-		return C.EC_BACKEND_JERASURE_RS_CAUCHY, nil
-	case "flat_xor_hd":
-		return C.EC_BACKEND_FLAT_XOR_HD, nil
-	case "isa_l_rs_vand":
-		return C.EC_BACKEND_ISA_L_RS_VAND, nil
-	case "shss":
-		return C.EC_BACKEND_SHSS, nil
-	case "liberasurecode_rs_vand":
-		return C.EC_BACKEND_LIBERASURECODE_RS_VAND, nil
-	case "isa_l_rs_cauchy":
-		return C.EC_BACKEND_ISA_L_RS_CAUCHY, nil
-	case "libphazr":
-		return C.EC_BACKEND_LIBPHAZR, nil
-	default:
-		return 0, errors.New(fmt.Sprintf("unsupported backend %q", name))
-	}
-}
-
-func errToName(errno C.int) string {
-	switch errno {
-	case C.EBACKENDNOTSUPP:
-		return "EBACKENDNOTSUPP"
-	case C.EECMETHODNOTIMPL:
-		return "EECMETHODNOTIMPL"
-	case C.EBACKENDINITERR:
-		return "EBACKENDINITERR"
-	case C.EBACKENDINUSE:
-		return "EBACKENDINUSE"
-	case C.EBACKENDNOTAVAIL:
-		return "EBACKENDNOTAVAIL"
-	case C.EBADCHKSUM:
-		return "EBADCHKSUM"
-	case C.EINVALIDPARAMS:
-		return "EINVALIDPARAMS"
-	case C.EBADHEADER:
-		return "EBADHEADER"
-	case C.EINSUFFFRAGS:
-		return "EINSUFFFRAGS"
-	default:
-		return fmt.Sprintf("<unknown error code %v>", errno)
-	}
-}
-
 type ErasureCodeBackend struct {
 	Name       string
 	K          int
